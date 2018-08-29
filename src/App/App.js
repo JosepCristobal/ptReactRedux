8983a35854aa0ba00278dfc534b047fcb373327e
Josep Cristobal <josepc.mercader@gmail.com>
2018-08-29 18:16:11 +0200

    Segunda subida

--- src/App/App.js
+++ src/App/App.js
@@ -13,16 +13,17 @@ import HomeTicTacContainer from '../Components/HomeTicTacContainer'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.State = {
-      turn: this.props.turno,
-      board: this.props.tablero,
-      totalMoves: this.props.movimientos,
-    };
-    this.gameState = {
+    this.state = {
       winner: undefined,
       winnerLine: 'El ganador es el jugador...',
+    };
+    this.gameState = {
+      turn: this.props.turno,
       gameLocked: false,
       gameEnded: false,
+      board: this.props.tablero,
+      totalMoves: this.props.movimientos,
+
     };
 
 
@@ -31,51 +32,53 @@ class App extends Component {
   clicked(box) {
 
     if (this.gameState.gameEnded || this.gameState.gameLocked) return;
-      this.State.turn = this.props.turno
-    if (this.State.board[box.dataset.square] === '') {
-      this.State.board[box.dataset.square] = this.gameState.turn;
-
-      box.innerText = this.State.turn;
+    //this.gameState.turn = this.props.turno
+    if (this.props.tablero[box.dataset.square] === '') {
+      //if (this.gameState.board[box.dataset.square] === '') {
+      //this.gameState.board[box.dataset.square] = this.props.turno;
+      //this.props.tablero[box.dataset.square] = this.props.turno;
+      //box.innerText = this.gameState.turn;
+      box.innerText = this.props.turno;
 
       //this.gameState.turn = this.gameState.turn === 'X' ? 'O' : 'X';
       //this.gameState.turn = this.props.turno
       const board2 = box.dataset.square;
       //const turn2 = this.gameState.turn ;
-      const turn2 = this.State.turn ;
+      const turn2 = this.gameState.turn ;
       //const turn2 = this.props.turno === 'X' ? JUGADOR0 : JUGADORX;
-      this.State.totalMoves++;
-      this.props.dispatch(jugarPosicion(board2, turn2));
+      this.gameState.totalMoves++;
+      this.props.dispatch(jugarPosicion(board2, this.props.turno));
       //mapStateToProps()
       console.log(this.props.movimientos);
       console.log(this.props.tablero);
-      console.log(this.State.board);
+      console.log(this.gameState.board);
     
       //this.gameState.totalMoves++;
 
       this.gameState.gameEnded = false;
-      /*this.setState({
+      this.setState({
         winner: undefined,
         winnerLine: 'El ganador es el jugador...'
-      });*/
+      });
     }
 
     var result = this.checkWinner();
 
     if (result === 'X') {
       this.gameState.gameEnded = true;
-      this.setgameState({
+      this.setState({
         winner: 'X',
         winnerLine: 'El ganador es el jugador X'
       });
     } else if (result === 'O') {
       this.gameState.gameEnded = true;
-      this.setgameState({
+      this.setState({
         winner: 'O',
         winnerLine: 'El ganador es el jugador O'
       });
     } else if (result === 'draw') {
       this.gameState.gameEnded = true;
-      this.setgameState({
+      this.setState({
         winner: 'draw',
         winnerLine: 'No hay ganador'
       })
@@ -85,13 +88,13 @@ class App extends Component {
 
   checkWinner() {
     var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
-    var board = this.State.board;
+    var board = this.gameState.board;
     for (let i = 0; i < moves.length; i++) {
       if (board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]])
         return board[moves[i][0]];
     }
 
-    if (this.State.totalMoves === 9) {
+    if (this.gameState.totalMoves === 9) {
       return 'draw';
     }
   }
@@ -102,18 +105,19 @@ class App extends Component {
       // Reseteamos reducers primero ya que estamos desarrollando en paralelo
       this.props.dispatch(reset());
       
-      this.State = {
+      this.gameState = {
         turn: 'X',
-        board: Array(9).fill(''),
-        totalMoves: 0,
-       
-      }
-
-      this.setgameState({
-        winner: undefined,
-        winnerLine: 'El ganador es el jugador...',
         gameLocked: false,
         gameEnded: false,
+        board: Array(9).fill(''),
+        totalMoves: 0,
+        winner: undefined,
+      }
+
+      this.setState({
+        winner: undefined,
+        winnerLine: 'El ganador es el jugador...'
+
       });
 
       for (let i = 0; i <= 8; i++) {
@@ -144,7 +148,7 @@ class App extends Component {
             <div className="square" data-square="7"></div>
             <div className="square" data-square="8"></div>
           </div>}
-        < div > <CounterValue value={this.State} /></div >
+        < div > <CounterValue value={this.gameState} /></div >
         <div id="status">{this.state.winnerLine}</div>
         <button type="button" onClick={this.resetCounter}>
           Reiniciar
